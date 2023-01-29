985e8b30d4af3234d222b210be677ead27e622ed
Ameer Hamza Khan <hamzbame@corp.contoursoftware.net>
2023-01-29 15:25:25 +0500

    footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './componets/footer/footer';
 import Header from './componets/header/header';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Footer />
     </div>
   );
 }
