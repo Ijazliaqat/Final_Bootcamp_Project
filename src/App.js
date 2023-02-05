760e8f076f8fad6260af3a68b5d53a796610ce39
ameerHamzaKhan <ameeerhamxakhan@gmail.com>
2023-02-05 14:49:46 +0500

    #comment changes in single product

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-// import { Route, Routes } from 'react-router-dom';
-// import AddCart from './componets/add-cart/add-cart';
-// import CreateProduct from './componets/add-products/add-products';
-// import AddProducts from './componets/add-products/add-products';
-// import Dashboard from './componets/Dashboard/dashboard';
-// import Footer from './componets/footer/footer';
+import { Route, Routes } from 'react-router-dom';
+import AddCart from './componets/add-cart/add-cart';
+import CreateProduct from './componets/add-products/add-products';
+import AddProducts from './componets/add-products/add-products';
+import Dashboard from './componets/Dashboard/dashboard';
+import Footer from './componets/footer/footer';
 import Header from './componets/header/header';
-// import HeroSection from './componets/hero-section/hero-section';
-// import History from './componets/history/history';
-// import AllProducts from './componets/our-products/all-products';
+import HeroSection from './componets/hero-section/hero-section';
+import History from './componets/history/history';
+import AllProducts from './componets/our-products/all-products';
 import SingleProducts from './componets/single-products/single-products';
-// import Users from './componets/users/users';
+import Users from './componets/users/users';
 import './App.css';
-// import SignIn from './componets/authentication/sign-in/sign-in';
-// import SignUp from './componets/authentication/sign-up/sign-up';
-// import Wishlists from './componets/wishlists/wishlists';
+import SignIn from './componets/authentication/sign-in/sign-in';
+import SignUp from './componets/authentication/sign-up/sign-up';
+import Wishlists from './componets/wishlists/wishlists';
 
 
 
@@ -21,7 +21,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <AddCart />
+      <AddCart />
       <Routes>
         <Route path='/' element={<Dashboard />} />
         <Route path='products' element={<AllProducts />} />
@@ -31,10 +31,9 @@ function App() {
         <Route path='signin' element={<SignIn />} />
         <Route path='signup' element={<SignUp />} />
         <Route path='wishlists' element={<Wishlists />} />
-      </Routes> */}
-        <SingleProducts/>
+      </Routes>
       {/* <Users /> */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
