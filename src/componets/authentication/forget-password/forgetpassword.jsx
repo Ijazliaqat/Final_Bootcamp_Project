7a3cbd7ec4fd1286046e9090d19be3e51fa701bb
aneeqgulzar <aneeqgulzaeonline@gmail.com>
2023-02-10 17:17:03 +0500

    changes in code

--- src/componets/authentication/forget-password/forgetpassword.jsx
+++ src/componets/authentication/forget-password/forgetpassword.jsx
@@ -1,29 +1,51 @@
 import React from 'react';
+import { useState } from 'react';
+
 
 
 function Forget() {
 
-  function clickHandler() {
-    console.log("Reset Pasword")
-  }
+
+  const [email, setEmail] = useState('');
+  console.log(email)
+   
+  function handleSubmit(e) {
+      e.preventDefault();
+
+        console.log('Your form submit.');
+      
+  };
+
+
+  function handleClick(e) {
+    e.preventDefault();
+
+     console.log('You click submit.');
+    
+};
+    
+    
   return (
     <main className="form-signin d-flex justify-content-center">
-  <form>
+  <form onSubmit={handleSubmit}>
     
-    <h1 class="h3 mt-5 mb-3 fw-normal justify-content-center">Forget Password</h1>
-    <h1 class="h6 mb-3 fw-normal">Enter you Registered email adress</h1>
+    <h1 className="h3 mt-5 mb-3 fw-normal justify-content-center">Forget Password</h1>
+    <h1 className="h6 mb-3 fw-normal">Enter you Registered email adress</h1>
 
-    <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
+    <div className="form-floating">
+      <input type="email" className="form-control" id="floatingInput" value={email} placeholder="name@example.com" onChange={(event) =>
+          setEmail(event.target.value)}/>
+          
       <label for="floatingInput">Email address</label>
     </div>
    
-    <button class="w-100 btn btn-lg btn-success mt-5" type="submit" onClick={clickHandler}>Reset Password</button>
-    <p class="mt-5 mb-3 ">Not a Member? <a href="">Signup for a account now</a></p>
+    <button className="w-100 btn btn-lg btn-success mt-5" value={email} type='submit' onClick={handleClick} >Reset Password</button>
+    <p className="mt-5 mb-3 ">Not a Member? <a href="">Signup for a account now</a></p>
+    
     
   </form>
 </main>
   );
-}
+  }
 
 export default Forget;
