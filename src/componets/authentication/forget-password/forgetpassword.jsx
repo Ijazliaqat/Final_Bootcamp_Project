f0954b47d60a2d6a66a04c6a85966c1242ea6b36
aneeqgulzar <aneeqgulzaeonline@gmail.com>
2023-02-11 12:31:48 +0500

    validation funtionality add

--- src/componets/authentication/forget-password/forgetpassword.jsx
+++ src/componets/authentication/forget-password/forgetpassword.jsx
@@ -7,45 +7,52 @@ function Forget() {
 
 
   const [email, setEmail] = useState('');
-  console.log(email)
-   
-  function handleSubmit(e) {
-      e.preventDefault();
 
-        console.log('Your form submit.');
-      
+  const [error, setError] = useState(null);
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email);
+
+    if (!validateEmail(email)) {
+      setError("Invalid email address");
+    } else {
+      setError(null);
+    }
   };
 
 
-  function handleClick(e) {
-    e.preventDefault();
 
-     console.log('You click submit.');
-    
-};
-    
-    
   return (
     <main className="form-signin d-flex justify-content-center">
-  <form onSubmit={handleSubmit}>
-    
-    <h1 className="h3 mt-5 mb-3 fw-normal justify-content-center">Forget Password</h1>
-    <h1 className="h6 mb-3 fw-normal">Enter you Registered email adress</h1>
+      <form onSubmit={handleSubmit}>
 
-    <div className="form-floating">
-      <input type="email" className="form-control" id="floatingInput" value={email} placeholder="name@example.com" onChange={(event) =>
-          setEmail(event.target.value)}/>
-          
-      <label for="floatingInput">Email address</label>
-    </div>
-   
-    <button className="w-100 btn btn-lg btn-success mt-5" value={email} type='submit' onClick={handleClick} >Reset Password</button>
-    <p className="mt-5 mb-3 ">Not a Member? <a href="">Signup for a account now</a></p>
-    
-    
-  </form>
-</main>
+        <h1 className="h3 mt-5 mb-3 fw-normal justify-content-center">Forget Password</h1>
+        <h1 className="h6 mb-3 fw-normal">Enter you Registered email adress</h1>
+
+        <div className="form-floating">
+          <input type="email" className="form-control" id="floatingInput" value={email} placeholder="name@example.com" onChange= {handleChange} />
+
+          <label for="floatingInput">Email address</label>
+        </div>
+        <button className="w-100 btn btn-lg btn-success mt-5" value={email} type='submit' >Reset Password</button>
+        {error && <p>{error}</p>}
+
+        <p className="mt-5 mb-3 ">Not a Member? <a href="">Signup for a account now</a></p>
+
+
+      </form>
+    </main>
   );
-  }
+  };
 
 export default Forget;
